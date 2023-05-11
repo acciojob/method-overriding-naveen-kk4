c2323903f34d849fd64d41b9ec330a71a33c5f24
naveen-kk4 <naveennig2001@gmail.com>
2023-05-11 15:20:36 +0530

    third commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
         B obj1 = new B();
-        System.out.println(obj1.meth());
+        obj1.meth();
+        obj1.meth();
 
 
 
